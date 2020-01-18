6bcd209aa7f13353f45aef939fb0e09324cd774d
chris113377 <chris113377@gmail.com>
2020-01-18 10:31:36 -0600

    Updates

---
+++ precache-manifest.345a532315a9dfc52234423703e7a9eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7b42f66274ec36aaf78ceee77362af",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "933d8c15fdf7a7308489",
+    "url": "/ToDo/static/css/main.e3d1b031.chunk.css"
+  },
+  {
+    "revision": "f2b888272ebcbfd6c26b",
+    "url": "/ToDo/static/js/2.24161841.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ToDo/static/js/2.24161841.chunk.js.LICENSE"
+  },
+  {
+    "revision": "933d8c15fdf7a7308489",
+    "url": "/ToDo/static/js/main.d66318d0.chunk.js"
+  },
+  {
+    "revision": "48ba9d4df14f2edeed25",
+    "url": "/ToDo/static/js/runtime-main.b363468a.js"
+  }
+]);
